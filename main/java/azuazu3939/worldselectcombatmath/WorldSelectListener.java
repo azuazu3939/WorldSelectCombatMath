a1ff20e81df888e7a53b9c05fe0323ec75e78628
azuazu3939 <98195581+azuazu3939@users.noreply.github.com>
2023-01-09 23:07:19 +0900

    Add files via upload

--- main/java/azuazu3939/worldselectcombatmath/WorldSelectListener.java
+++ main/java/azuazu3939/worldselectcombatmath/WorldSelectListener.java
@@ -3,7 +3,9 @@ package azuazu3939.worldselectcombatmath;
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -17,87 +19,96 @@ public class WorldSelectListener implements Listener {
     @EventHandler
     public void onDamaged(EntityDamageByEntityEvent event) {
 
-        if (!(event.getEntity() instanceof LivingEntity damaged)) return;
+        if (event.getEntity() instanceof LivingEntity damaged) {
 
-        for (String worldName : WorldSelectCombatMath.inst().getConfig().getStringList("WorldName")) {
-            if (damaged.getWorld().getName().equals(worldName)) {
+            for (String worldName : WorldSelectCombatMath.inst().getConfig().getStringList("WorldName")) {
+                if (damaged.getWorld().getName().equals(worldName)) {
 
-                double damage = event.getDamage();
-                int armor = 0;
-                double toughness = 0;
+                    double damage = event.getDamage();
+                    int armor = 0;
+                    double toughness = 0;
 
-                if (damaged.getAttribute(Attribute.GENERIC_ARMOR) != null) {
-                    armor = (int) Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR)).getValue();
-                }
-                if (damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS) != null) {
-                    toughness = Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS)).getValue();
-                }
-
-                double finalDamageMath = (damage / (1 + (armor * 0.05))) - toughness;
-                if (finalDamageMath <= 0) {
-                    event.setCancelled(true);
-                } else {
-
-                    if (damaged.hasPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)) {
-                        finalDamageMath = finalDamageMath * (1 - 0.2 * (Objects.requireNonNull(damaged.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)).getAmplifier() + 1));
+                    if (damaged.getAttribute(Attribute.GENERIC_ARMOR) != null) {
+                        armor = (int) Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR)).getValue();
                     }
+                    if (damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS) != null) {
+                        toughness = Objects.requireNonNull(damaged.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS)).getValue();
+                    }
+
+                    double enchant = 1;
                     if (damaged.getEquipment() != null) {
                         int level = 0;
-                        if (damaged.getEquipment().getBoots() != null &&
+                        if (damaged.getEquipment().getBoots() != null && damaged.getEquipment().getBoots().hasItemMeta() &&
                                 Objects.requireNonNull(damaged.getEquipment().getBoots().getItemMeta()).hasEnchant(Enchantment.PROTECTION_ENVIRONMENTAL)) {
                             level += damaged.getEquipment().getBoots().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
                         }
-                        if (damaged.getEquipment().getBoots() != null && event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
+                        if (damaged.getEquipment().getBoots() != null && damaged.getEquipment().getBoots().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
                                 Objects.requireNonNull(damaged.getEquipment().getBoots().getItemMeta()).hasEnchant(Enchantment.PROTECTION_PROJECTILE)) {
                             level += damaged.getEquipment().getBoots().getEnchantmentLevel(Enchantment.PROTECTION_PROJECTILE) * 2;
                         }
-                        if (damaged.getEquipment().getBoots() != null && event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
+                        if (damaged.getEquipment().getBoots() != null && damaged.getEquipment().getBoots().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
                                 Objects.requireNonNull(damaged.getEquipment().getBoots().getItemMeta()).hasEnchant(Enchantment.PROTECTION_EXPLOSIONS)) {
                             level += damaged.getEquipment().getBoots().getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) * 2;
                         }
-                        if (damaged.getEquipment().getLeggings() != null &&
+                        if (damaged.getEquipment().getLeggings() != null && damaged.getEquipment().getLeggings().hasItemMeta() &&
                                 Objects.requireNonNull(damaged.getEquipment().getLeggings().getItemMeta()).hasEnchant(Enchantment.PROTECTION_ENVIRONMENTAL)) {
                             level += damaged.getEquipment().getLeggings().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
                         }
-                        if (damaged.getEquipment().getLeggings() != null && event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
+                        if (damaged.getEquipment().getLeggings() != null && damaged.getEquipment().getLeggings().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
                                 Objects.requireNonNull(damaged.getEquipment().getLeggings().getItemMeta()).hasEnchant(Enchantment.PROTECTION_PROJECTILE)) {
                             level += damaged.getEquipment().getLeggings().getEnchantmentLevel(Enchantment.PROTECTION_PROJECTILE) * 2;
                         }
-                        if (damaged.getEquipment().getLeggings() != null && event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
+                        if (damaged.getEquipment().getLeggings() != null && damaged.getEquipment().getLeggings().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
                                 Objects.requireNonNull(damaged.getEquipment().getLeggings().getItemMeta()).hasEnchant(Enchantment.PROTECTION_EXPLOSIONS)) {
                             level += damaged.getEquipment().getLeggings().getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) * 2;
                         }
-                        if (damaged.getEquipment().getChestplate() != null &&
+                        if (damaged.getEquipment().getChestplate() != null && damaged.getEquipment().getChestplate().hasItemMeta() &&
                                 Objects.requireNonNull(damaged.getEquipment().getChestplate().getItemMeta()).hasEnchant(Enchantment.PROTECTION_ENVIRONMENTAL)) {
                             level += damaged.getEquipment().getChestplate().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
                         }
-                        if (damaged.getEquipment().getChestplate() != null && event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
+                        if (damaged.getEquipment().getChestplate() != null && damaged.getEquipment().getChestplate().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
                                 Objects.requireNonNull(damaged.getEquipment().getChestplate().getItemMeta()).hasEnchant(Enchantment.PROTECTION_PROJECTILE)) {
                             level += damaged.getEquipment().getChestplate().getEnchantmentLevel(Enchantment.PROTECTION_PROJECTILE) * 2;
                         }
-                        if (damaged.getEquipment().getChestplate() != null && event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
+                        if (damaged.getEquipment().getChestplate() != null && damaged.getEquipment().getChestplate().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
                                 Objects.requireNonNull(damaged.getEquipment().getChestplate().getItemMeta()).hasEnchant(Enchantment.PROTECTION_EXPLOSIONS)) {
                             level += damaged.getEquipment().getChestplate().getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) * 2;
                         }
-                        if (damaged.getEquipment().getHelmet() != null &&
+                        if (damaged.getEquipment().getHelmet() != null && damaged.getEquipment().getHelmet().hasItemMeta() &&
                                 Objects.requireNonNull(damaged.getEquipment().getHelmet().getItemMeta()).hasEnchant(Enchantment.PROTECTION_ENVIRONMENTAL)) {
                             level += damaged.getEquipment().getHelmet().getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
                         }
-                        if (damaged.getEquipment().getHelmet() != null && event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
+                        if (damaged.getEquipment().getHelmet() != null && damaged.getEquipment().getHelmet().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.PROJECTILE) &&
                                 Objects.requireNonNull(damaged.getEquipment().getHelmet().getItemMeta()).hasEnchant(Enchantment.PROTECTION_PROJECTILE)) {
                             level += damaged.getEquipment().getHelmet().getEnchantmentLevel(Enchantment.PROTECTION_PROJECTILE) * 2;
                         }
-                        if (damaged.getEquipment().getHelmet() != null && event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
+                        if (damaged.getEquipment().getHelmet() != null && damaged.getEquipment().getHelmet().hasItemMeta() &&
+                                event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) &&
                                 Objects.requireNonNull(damaged.getEquipment().getHelmet().getItemMeta()).hasEnchant(Enchantment.PROTECTION_EXPLOSIONS)) {
                             level += damaged.getEquipment().getHelmet().getEnchantmentLevel(Enchantment.PROTECTION_EXPLOSIONS) * 2;
                         }
                         if (level > 20) level = 20;
                         if (level != 0) {
-                            finalDamageMath = finalDamageMath * (1 - level * 0.04);
+                            enchant = (1 - level * 0.01);
                         }
                     }
-                    event.setDamage(0.0);
-                    event.setDamage(EntityDamageEvent.DamageModifier.ARMOR, finalDamageMath);
+                    double finalDamageMath = (damage * (1 - (armor * 0.01)) * (enchant)) - (toughness * 0.1);
+                    if (finalDamageMath <= 0) {
+                        event.setCancelled(true);
+                    } else {
+                        if (damaged.hasPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)) {
+                            finalDamageMath = finalDamageMath * (1 - 0.2 * (Objects.requireNonNull(damaged.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE)).getAmplifier() + 1));
+                        }
+                        event.setDamage(0.0);
+                        event.setDamage(EntityDamageEvent.DamageModifier.ARMOR, finalDamageMath);
+                    }
                 }
             }
         }
